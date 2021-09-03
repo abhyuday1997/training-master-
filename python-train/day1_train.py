1eee08d55a55efb9b8dac4ad473039fc3c377f95
unknown <abhyuday_suraparaju@kctechservices.com>
2021-09-03 12:32:33 +0530

    Python_file_formatted

---
+++ python-train/day1_train.py
@@ -0,0 +1,23 @@
+print('Hello Python GCIT world!')
+multiline_string = '''Myself_Abhyuday
+preparing my kernel for '\\'
+data_engineering.
+'''
+print(multiline_string)
+
+
+first_name = 'Abhi'
+
+print(first_name)
+print(first_name.title())
+print(first_name.upper())
+print(first_name.lower())
+
+first_name = 'Abhyuday'
+last_name = 'suraparaju'
+full_name = first_name + ' ' + last_name
+
+message = full_name.title() + ' ' + \
+        "was performing programming for data engieering at GCIT."
+
+print(message)
